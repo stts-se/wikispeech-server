0a4e94e1eb2e90adcf7a64698b88f5d47ad4710c
Harald Berthelsen <harald@stts.se>
2016-05-17 14:51:47 +0200

    more tests

--- test/test_api.py
+++ test/test_api.py
@@ -139,41 +139,190 @@ test_done()
 
 #2) textprocessing api
 
+host = "http://localhost:10000/wikispeech/textprocessing/"
+
 # 2.1
-# GET:  curl -X OPTIONS "http://localhost:10000/wikispeech/textprocessing/"
-# TODO Currently nothing. Should return list of allowed parameters
+# OPTIONS:  curl -X OPTIONS "http://localhost:10000/wikispeech/textprocessing/"
+# TODO Currently nothing. Should return list of allowed parameters?
+
+r = requests.options("%s" % (host))
+res = r.text
+expected = ""
+assert ( res == expected ), "%s != %s" % (res, expected)
+test_done()
 
 # 2.2
 # GET:  curl "http://localhost:10000/wikispeech/textprocessing/"
 # Returns a (bad) error message
 # TODO Better message, with suggested usage? Similar to /wikispeech/
 
+r = requests.get("%s" % (host))
+res = r.text
+expected = "ERROR: No textprocessor available for language None"
+assert ( res == expected ), "%s != %s" % (res, expected)
+test_done()
+
 # 2.3
 # GET:  curl "http://localhost:10000/wikispeech/textprocessing/languages"
-# returns list of supported languages
+# returns list of textprocessors for all supported languages
+
+r = requests.get("%slanguages" % (host))
+res = r.json()
+assert ( type(res) == type([]) )
+
+expected = ["sv", "en", "ar"]
+for textprocessor in res:    
+    assert ( textprocessor["lang"] in expected ), "%s not in  %s" % (textprocessor["lang"], expected)
+test_done()
+
 
 # 2.4
 # GET:  curl "http://localhost:10000/wikispeech/textprocessing/languages/sv"
 # returns list of defined textprocessors for <lang>
 
+r = requests.get("%slanguages/sv" % (host))
+res = r.json()
+assert ( type(res) == type([]) )
+expected = ["wikitextproc_sv"]
+for textprocessor in res:    
+    assert ( textprocessor["name"] in expected ), "%s not in %s" % (textprocessor["name"], expected)
+test_done()
+
 # 2.5
 # GET:  curl "http://localhost:10000/wikispeech/textprocessing/?lang=en&input=test."
 # returns json markup
+
+payload = {
+    "lang":"en",
+    "input":"test."
+}
+
+r = requests.get("%s" % (host), params=payload)
+res = r.json()
+assert ( type(res) == type({}) )
+
+expected = {"children": [{"children": [{"children": [{"children": [{"accent": "!H*", "children": [{"accent": "!H*", "children": [{"p": "t", "tag": "ph"}, {"p": "E", "tag": "ph"}, {"p": "s", "tag": "ph"}, {"p": "t", "tag": "ph"}], "ph": "t E s t", "stress": "1", "tag": "syllable"}], "g2p_method": "lexicon", "ph": "' t E s t", "pos": "NN", "tag": "t", "text": "test"}, {"pos": ".", "tag": "t", "text": "."}, {"breakindex": "5", "tag": "boundary", "tone": "L-L%"}], "tag": "phrase"}], "tag": "s"}], "tag": "p"}], "tag": "utt"}
+
+assert ( res == expected ), "%s != %s" % (res, expected)
+test_done()
 
 # 2.6
 # textprocessor can be an argument, returns message if not defined
 # GET:  curl "http://localhost:10000/wikispeech/textprocessing/?lang=en&input=test.&textprocessor=undefined"
 
+payload = {
+    "lang":"en",
+    "input":"test.",
+    "textprocessor":"undefined"
+}
+
+r = requests.get("%s" % (host), params=payload)
+res = r.text
+assert ( type(res) == type("") )
+
+expected = "ERROR: Textprocessor undefined not defined for language en"
+
+assert ( res == expected ), "%s != %s" % (res, expected)
+test_done()
 
 
 #3) synthesis api
 
+host = "http://localhost:10000/wikispeech/synthesis/"
+
+# 3.1
+# OPTIONS:  curl -X OPTIONS "http://localhost:10000/wikispeech/synthesis/"
+# TODO Currently nothing. Should return list of allowed parameters?
+
+r = requests.options("%s" % (host))
+res = r.text
+expected = ""
+assert ( res == expected ), "%s != %s" % (res, expected)
+test_done()
+
+# 3.2
 # GET:  curl "http://localhost:10000/wikispeech/synthesis/"
+# Returns a (bad) error message
+# TODO Better message, with suggested usage? Similar to /wikispeech/
+
+r = requests.get("%s" % (host))
+res = r.text
+expected = "synthesis does not support language None"
+assert ( res == expected ), "%s != %s" % (res, expected)
+test_done()
+
+
+# 3.3
 # GET:  curl "http://localhost:10000/wikispeech/synthesis/voices"
+# returns list of voices for all supported languages
+
+r = requests.get("%svoices" % (host))
+res = r.json()
+assert ( type(res) == type([]) )
+
+expected = ["sv", "en", "ar"]
+for voice in res:    
+    assert ( voice["lang"] in expected ), "%s not in  %s" % (voice["lang"], expected)
+test_done()
+
+
+# 3.4
 # GET:  curl "http://localhost:10000/wikispeech/synthesis/voices/sv"
+# returns list of defined voicess for <lang>
+
+r = requests.get("%svoices/sv" % (host))
+res = r.json()
+assert ( type(res) == type([]) )
+expected = ["stts_sv_nst-hsmm", "espeak_mbrola_sv1"]
+for voice in res:    
+    assert ( voice["name"] in expected ), "%s not in %s" % (voice["name"], expected)
+test_done()
+
+# 3.5
 # GET:  curl "http://localhost:10000/wikispeech/synthesis/?lang=en&input=test."
+# returns json, with "audio" url and "tokens" list
+markup = {"children": [{"children": [{"children": [{"children": [{"accent": "!H*", "children": [{"accent": "!H*", "children": [{"p": "t", "tag": "ph"}, {"p": "E", "tag": "ph"}, {"p": "s", "tag": "ph"}, {"p": "t", "tag": "ph"}], "ph": "t E s t", "stress": "1", "tag": "syllable"}], "g2p_method": "lexicon", "ph": "' t E s t", "pos": "NN", "tag": "t", "text": "test"}, {"pos": ".", "tag": "t", "text": "."}, {"breakindex": "5", "tag": "boundary", "tone": "L-L%"}], "tag": "phrase"}], "tag": "s"}], "tag": "p"}], "tag": "utt"}
+
+payload = {
+    "lang":"en",
+    "input":json.dumps(markup)
+}
+
+
+r = requests.get("%s" % (host), params=payload)
+res = r.json()
+assert ( type(res) == type({}) )
+
+expected = {"audio": "http://morf.se:59125/process?INPUT_TYPE=ALLOPHONES&LOCALE=en&INPUT_TEXT=%3C%3Fxml+version%3D%221.0%22+encoding%3D%22UTF-8%22%3F%3E%3Cmaryxml+version%3D%220.5%22+xml%3Alang%3D%22en%22+xmlns%3D%22http%3A%2F%2Fmary.dfki.de%2F2002%2FMaryXML%22+xmlns%3Axsi%3D%22http%3A%2F%2Fwww.w3.org%2F2001%2FXMLSchema-instance%22%3E%3Cp%3E%3Cs%3E%3Cphrase%3E%3Ct+accent%3D%22%21H%2A%22+g2p_method%3D%22lexicon%22+ph%3D%22%27+t+E+s+t%22+pos%3D%22NN%22%3Etest%3Csyllable+accent%3D%22%21H%2A%22+ph%3D%22t+E+s+t%22+stress%3D%221%22%3E%3Cph+p%3D%22t%22+%2F%3E%3Cph+p%3D%22E%22+%2F%3E%3Cph+p%3D%22s%22+%2F%3E%3Cph+p%3D%22t%22+%2F%3E%3C%2Fsyllable%3E%3C%2Ft%3E%3Ct+pos%3D%22.%22%3E.%3C%2Ft%3E%3Cboundary+breakindex%3D%225%22+tone%3D%22L-L%25%22+%2F%3E%3C%2Fphrase%3E%3C%2Fs%3E%3C%2Fp%3E%3C%2Fmaryxml%3E&VOICE=dfki-spike-hsmm&AUDIO=WAVE_FILE&OUTPUT_TYPE=AUDIO", "tokens": [["test", 0.465], [".", 0.465], ["", 1.265]]}
+
+assert ( res == expected ), "%s != %s" % (res, expected)
+test_done()
+
+
+# 3.6
 # GET:  curl "http://localhost:10000/wikispeech/synthesis/?lang=en&input=test.&voice=cmu-slt-flite"
-# GET:  curl "http://localhost:10000/wikispeech/synthesis/?lang=en&input=test.&voice=undefined"
+# GET:  curl "http://localhost:10000/wikispeech/synthesis/?lang=en&input=\{\}&voice=undefined"
+# voice can be an argument, returns message if not defined
+
+#TODO this also fails
+
+payload = {
+    "lang":"en",
+    "input":markup,
+    "voice":"undefined"
+}
+
+r = requests.get("%s" % (host), params=payload)
+res = r.text
+assert ( type(res) == type("") )
+
+expected = "ERROR: voice undefined not defined for language en."
+
+assert ( res == expected ), "%s != %s" % (res, expected)
+test_done()
+
+
+#######################################################
 
 
 all_done()
