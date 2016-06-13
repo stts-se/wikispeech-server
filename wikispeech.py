192230727a045e7cae22ac1f8cc1b98a733b5563
Harald Berthelsen <harald@stts.se>
2016-06-13 12:47:13 +0200

    Updated OPTIONS

--- wikispeech.py
+++ wikispeech.py
@@ -27,7 +27,7 @@ CORS(app)
 def wikispeech_options():
 
     options = {
-        "GET|POST": {
+        "GET": {
             "description": "Get speech from text",
             "parameters": {
                 "input": {
@@ -89,9 +89,13 @@ def wikispeech_options():
     }
                 
 
+    #Parameters for POST are the same as for GET. If they're not, "POST" needs to be defined separately!
+    options["POST"] = options["GET"]
+
+
     resp = make_response(json.dumps(options))
     resp.headers["Content-type"] = "application/json"
-    resp.headers["Allow"] = "OPTIONS, GET, POST"
+    resp.headers["Allow"] = "OPTIONS, GET, POST, HEAD"
     return resp
 
 
@@ -209,6 +213,80 @@ def list_tp_configs_by_language(lang):
         if tp_config["lang"] == lang:
             l.append(tp_config)
     return l
+
+
+
+
+
+@app.route('/wikispeech/textprocessing/', methods=["OPTIONS"])
+def textprocessing_options():
+
+    options = {
+        "GET": {
+            "description": "Get markup from text",
+            "parameters": {
+                "input": {
+                    "type": "string",
+                    "description": "The text to be processed",
+                    "required": True,
+                    "default": None
+                },
+                "lang": {
+                    "type": "string",
+                    "description": "ISO 639-1 two-letter code for textprocessing language",
+                    "required": True,
+                    "allowed": textprocSupportedLanguages(),
+                    "default": None
+                },
+                "textprocessor": {
+                    "type": "string",
+                    "description": "name of a defined wikispeech textprocessor for this language",
+                    "required": False,
+                    "default": "The default textprocessor for this language"
+                },
+                "input_type": {
+                    "type": "string",
+                    "description": "the type of the input, for instance with or without markup",
+                    "required": False,
+                    "allowed": "text",
+                    "default": "text"
+                },
+                "ouput_type": {
+                    "type": "string",
+                    "description": "the type of the output. Only json implemented so meaningless at the moment",
+                    "required": False,
+                    "allowed": "json",
+                    "default": "json"
+                }                
+            },
+            "examples": [
+                {
+                    "input": "Det här är ett test",
+                    "lang": "sv"
+                },
+                {
+                    "input": "Det här är ett test",
+                    "lang": "sv",
+                    "textprocessor": "wikitextproc_sv",
+                    "input_type": "text",
+                    "output_type": "json"
+                }
+            ]
+            
+        }
+    }
+                
+
+    #Parameters for POST are the same as for GET. If they're not, "POST" needs to be defined separately!
+    options["POST"] = options["GET"]
+
+
+    resp = make_response(json.dumps(options))
+    resp.headers["Content-type"] = "application/json"
+    resp.headers["Allow"] = "OPTIONS, GET, POST, HEAD"
+    return resp
+
+
 
 @app.route('/wikispeech/textprocessing/', methods=["GET", "POST"])
 def textprocessing():
@@ -335,6 +413,77 @@ def synthesisSupportedLanguages():
             langs.append(voice["lang"])
     return langs
                         
+
+
+@app.route('/wikispeech/synthesis/', methods=["OPTIONS"])
+def synthesis_options():
+
+    options = {
+        "GET": {
+            "description": "Get speech from markup",
+            "parameters": {
+                "input": {
+                    "type": "string",
+                    "description": "The markup to be synthesised",
+                    "required": True,
+                    "default": None
+                },
+                "lang": {
+                    "type": "string",
+                    "description": "ISO 639-1 two-letter code for synthesis language",
+                    "required": True,
+                    "allowed": synthesisSupportedLanguages(),
+                    "default": None
+                },
+                "voice": {
+                    "type": "string",
+                    "description": "name of a defined wikispeech voice for this language",
+                    "required": False,
+                    "default": "The default voice for this language"
+                },
+                "input_type": {
+                    "type": "string",
+                    "description": "the type of the input. Only 'markup' implemented, so currently meaningless",
+                    "required": False,
+                    "allowed": "markup",
+                    "default": "markup"
+                },
+                "output_type": {
+                    "type": "string",
+                    "description": "the type of the output, for instance with or without timing information",
+                    "required": False,
+                    "allowed": "json",
+                    "default": "json"
+                }                
+            },
+            "examples": [
+                {
+                    "input": {"children": [{"children": [{"children": [{"children": [{"accent": "!H*", "children": [{"accent": "!H*", "children": [{"p": "t", "tag": "ph"}, {"p": "E", "tag": "ph"}, {"p": "s", "tag": "ph"}, {"p": "t", "tag": "ph"}], "ph": "t E s t", "stress": "1", "tag": "syllable"}], "g2p_method": "lexicon", "ph": "' t E s t", "pos": "NN", "tag": "t", "text": "test"}, {"pos": ".", "tag": "t", "text": "."}, {"breakindex": "5", "tag": "boundary", "tone": "L-L%"}], "tag": "phrase"}], "tag": "s"}], "tag": "p"}], "tag": "utt"},
+                    "lang": "sv"
+                },
+                {
+                    "input": {"children": [{"children": [{"children": [{"children": [{"accent": "!H*", "children": [{"accent": "!H*", "children": [{"p": "t", "tag": "ph"}, {"p": "E", "tag": "ph"}, {"p": "s", "tag": "ph"}, {"p": "t", "tag": "ph"}], "ph": "t E s t", "stress": "1", "tag": "syllable"}], "g2p_method": "lexicon", "ph": "' t E s t", "pos": "NN", "tag": "t", "text": "test"}, {"pos": ".", "tag": "t", "text": "."}, {"breakindex": "5", "tag": "boundary", "tone": "L-L%"}], "tag": "phrase"}], "tag": "s"}], "tag": "p"}], "tag": "utt"},
+                    "lang": "sv",
+                    "voice": "stts_sv_nst-hsmm",
+                    "input_type": "markup",
+                    "output_type": "json"
+                }
+            ]
+            
+        }
+    }
+                
+
+    #Parameters for POST are the same as for GET. If they're not, "POST" needs to be defined separately!
+    options["POST"] = options["GET"]
+
+
+    resp = make_response(json.dumps(options))
+    resp.headers["Content-type"] = "application/json"
+    resp.headers["Allow"] = "OPTIONS, GET, POST, HEAD"
+    return resp
+
+
 
 
 @app.route('/wikispeech/synthesis/', methods=["GET","POST"])
