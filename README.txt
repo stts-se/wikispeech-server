e5db33186aa6f538cbbdf76fe3844ce457788310
Harald Berthelsen <harald@stts.se>
2017-03-29 16:25:40 +0200

    initial setup.py

---
+++ README.txt
@@ -0,0 +1,9 @@
+server for wikispeech tts.
+
+
+Install:
+python3 setup.py install
+
+Usage:
+python3 wikispeech.py
+
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='wikispeech_mockup',
+      version='0.1',
+      description='Server for wikispeech tts',
+      url='https://github.com/HaraldBerthelsen/wikispeech_mockup',
+      author='Harald Berthelsen',
+      author_email='haraldberthelsen@gmail.com',
+      license='MIT',
+      packages=['wikispeech_mockup'],
+      install_requires=[
+          'requests',
+          'flask',
+          'flask_cors',
+      ],
+      zip_safe=False)
