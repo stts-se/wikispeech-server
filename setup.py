bf79b454226e824b01c76d2aa4f3572352ca6407
Harald Berthelsen <harald@stts.se>
2017-03-31 10:37:25 +0200

    distutils instead of setuptools

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
-from setuptools import setup
+#from setuptools import setup
+from distutils.core import setup
 
+#for distutils
 setup(name='wikispeech_mockup',
       version='0.1',
       description='Server for wikispeech tts',
@@ -11,11 +13,34 @@ setup(name='wikispeech_mockup',
           'wikispeech_mockup',
           'wikispeech_mockup.adapters',
       ],
-      install_requires=[
+      requires=[
           'requests',
           'flask',
           'flask_cors',
       ],
-      scripts=['bin/wikispeech_server', 'bin/wikispeech'],
-      include_package_data=True,
-      zip_safe=False)
+      scripts=['bin/wikispeech_server', 'bin/wikispeech']
+#      include_package_data=True,
+#      zip_safe=False
+)
+
+# for setuptools
+# setup(name='wikispeech_mockup',
+#       version='0.1',
+#       description='Server for wikispeech tts',
+#       url='https://github.com/HaraldBerthelsen/wikispeech_mockup',
+#       author='Harald Berthelsen',
+#       author_email='haraldberthelsen@gmail.com',
+#       license='MIT',
+#       packages=[
+#           'wikispeech_mockup',
+#           'wikispeech_mockup.adapters',
+#       ],
+#       install_requires=[
+#           'requests',
+#           'flask',
+#           'flask_cors',
+#       ],
+#       scripts=['bin/wikispeech_server', 'bin/wikispeech'],
+#       include_package_data=True,
+#       zip_safe=False)
+
