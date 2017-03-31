9c46828a837fb43cbec59c43bdee6ee4c4431a79
Harald Berthelsen <harald@stts.se>
2017-03-31 10:55:44 +0200

    added check for installed setuptools

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
-#from setuptools import setup
-from distutils.core import setup
+try:
+    from setuptools import setup
+except:
+    print("setuptools was not found. You should probably run something like\nsudo apt-get install python3-setuptools")
+    import sys
+    sys.exit(1)
+    
 
-#for distutils
 setup(name='wikispeech_mockup',
       version='0.1',
       description='Server for wikispeech tts',
@@ -13,34 +17,12 @@ setup(name='wikispeech_mockup',
           'wikispeech_mockup',
           'wikispeech_mockup.adapters',
       ],
-      requires=[
+      install_requires=[
           'requests',
           'flask',
           'flask_cors',
       ],
-      scripts=['bin/wikispeech_server', 'bin/wikispeech']
-#      include_package_data=True,
-#      zip_safe=False
-)
-
-# for setuptools
-# setup(name='wikispeech_mockup',
-#       version='0.1',
-#       description='Server for wikispeech tts',
-#       url='https://github.com/HaraldBerthelsen/wikispeech_mockup',
-#       author='Harald Berthelsen',
-#       author_email='haraldberthelsen@gmail.com',
-#       license='MIT',
-#       packages=[
-#           'wikispeech_mockup',
-#           'wikispeech_mockup.adapters',
-#       ],
-#       install_requires=[
-#           'requests',
-#           'flask',
-#           'flask_cors',
-#       ],
-#       scripts=['bin/wikispeech_server', 'bin/wikispeech'],
-#       include_package_data=True,
-#       zip_safe=False)
+      scripts=['bin/wikispeech_server', 'bin/wikispeech'],
+      include_package_data=True,
+      zip_safe=False)
 
