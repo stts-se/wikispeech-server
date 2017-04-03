8976f3cfd93e383140a186bed7c437cbe2ef0efc
Harald Berthelsen <harald@stts.se>
2017-04-03 10:07:26 +0200

    path to config files

--- wikispeech_mockup/config.py
+++ wikispeech_mockup/config.py
@@ -10,15 +10,24 @@ config = configparser.SafeConfigParser()
 
 #default_config_file = "%s/git/wikispeech_mockup/wikispeech_mockup/default.conf" % home
 default_config_file = "wikispeech_mockup/default.conf"
+
+if not os.path.isfile(default_config_file):
+    print("ERROR: Default config file %s not found" % default_config_file)
+    sys.exit(1)
+
 print("Default config file: %s" % default_config_file)
 config.read(default_config_file)
 
 #user_config_file = "%s/git/wikispeech_mockup/wikispeech_mockup/%s-%s.conf" % (home,user, hostname)
 user_config_file = "wikispeech_mockup/%s-%s.conf" % (user, hostname)
-print("User config file: %s" % user_config_file)
+if not os.path.isfile(user_config_file):
+    print("User config file %s not found" % user_config_file)
+else:
+    print("User config file: %s" % user_config_file)
+    config.read(user_config_file)
 
-config.read(user_config_file)
-
+    
 print("Testing config: Services|lexicon = %s" % config.get("Services", "lexicon"))
 
-sys.exit()
+
+
