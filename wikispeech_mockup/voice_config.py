77401453f1d1f2adc7b957907d251724ca3eac61
Harald Berthelsen <harald@stts.se>
2017-04-12 11:36:41 +0200

    removed 'server' from config file

--- wikispeech_mockup/voice_config.py
+++ wikispeech_mockup/voice_config.py
@@ -82,21 +82,20 @@ voices = [
         "mapper": {
             "from":"sv-se_ws-sampa",
             "to":"sv-se_sampa_mary"
-            },
-        "server":{"url":'https://demo.morf.se/marytts'}
+            }
     }
     ,
     #TODO
     #{"lang":"sv", "name":"espeak_mbrola_sv1", "engine":"espeak", "adapter":"adapters.espeak_adapter", "espeak_mbrola_voice":"mb-sw1", "espeak_voice":"mb-sw1", "program":{"command":'espeak -v mb-sw1'}}
     #,
 
-    {"lang":"nb", "name":"stts_no_nst-hsmm", "engine":"marytts", "adapter":"adapters.marytts_adapter", "marytts_locale":"no", "server":{"url":'https://demo.morf.se/marytts'}}
+    {"lang":"nb", "name":"stts_no_nst-hsmm", "engine":"marytts", "adapter":"adapters.marytts_adapter", "marytts_locale":"no"}
     ,
 
 
-    {"lang":"en", "name":"dfki-spike-hsmm", "engine":"marytts", "adapter":"adapters.marytts_adapter", "server":{"url":'https://demo.morf.se/marytts'}, "marytts_locale":"en_US"}
+    {"lang":"en", "name":"dfki-spike-hsmm", "engine":"marytts", "adapter":"adapters.marytts_adapter", "marytts_locale":"en_US"}
     ,
-    {"lang":"en", "name":"cmu-slt-hsmm", "engine":"marytts", "adapter":"adapters.marytts_adapter", "server":{"url":'https://demo.morf.se/marytts'}, "marytts_locale":"en_US"}
+    {"lang":"en", "name":"cmu-slt-hsmm", "engine":"marytts", "adapter":"adapters.marytts_adapter", "marytts_locale":"en_US"}
     ,
 
     #{"lang":"en", "name":"cmu_slt_hts", "engine":"hts_engine", "adapter":"hts_engine_adapter", "voice_file":"voices/hts_engine/cmu_us_arctic_slt_demo.htsvoice"}
@@ -114,7 +113,6 @@ voices = [
         "mapper": {
             "from":"ar_ws-sampa",
             "to":"ar_sampa_mary"
-        },
-        "server":{"url":'https://demo.morf.se/marytts'}
+        }
     }    
 ]
