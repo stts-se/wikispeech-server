e267e9f63c8ed83916bdd665a88ca2d12f5de5e3
Hanna Lindgren <han.lind@gmail.com>
2017-09-01 15:01:58 +0200

    docker

--- docker/marytts/docker_run.sh
+++ docker/marytts/docker_run.sh
@@ -1,1 +1,9 @@
-docker run --name=marytts --network=wikispeech -p 59125:59125 -ti marytts $*
+CNAME="marytts"
+if docker container inspect $CNAME &> /dev/null ; then
+    echo -n "STOPPING CONTAINER "
+    docker stop $CNAME
+    echo -n "DELETING CONTAINER "
+    docker rm $CNAME
+fi
+
+docker run --name=$CNAME --network=wikispeech -p 59125:59125 -ti marytts $*
--- docker/wikispeech/docker_run.sh
+++ docker/wikispeech/docker_run.sh
@@ -1,6 +1,14 @@
-NETWORKARGS="--network=wikispeech" # --name=pronlexSend --env SEND_HOST=pronlexListen --env SEND_PORT=8787"
-docker run --name=wikispeech $NETWORKARGS -p 10000:10000 -v /home/hanna/git_repos/wikispeech_mockup/wikispeech_server/:/config/ -it wikispeech_server $*
+CNAME="wikispeech"
+if docker container inspect $CNAME &> /dev/null ; then
+    echo -n "STOPPING CONTAINER "
+    docker stop $CNAME
+    echo -n "DELETING CONTAINER "
+    docker rm $CNAME
+fi
 
-## TODO: SNYGGARE MAPPNING AV CONFIGFILER!
+NETWORKARGS="--network=wikispeech"
+docker run --name=$CNAME $NETWORKARGS -p 10000:10000 -v /home/hanna/git_repos/wikispeech_mockup/wikispeech_server/:/config/ -it wikispeech $*
+
+## TODO: SNYGGARE MAPPNING AV CONFIGFILER
 
 
