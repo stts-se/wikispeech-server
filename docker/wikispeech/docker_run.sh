47ac63c8095fb54f23f73c6fe7df6ae0c8b55d71
Hanna Lindgren <han.lind@gmail.com>
2017-09-04 15:11:45 +0200

    r

--- docker/wikispeech/docker_run.sh
+++ docker/wikispeech/docker_run.sh
@@ -6,8 +6,7 @@ if docker container inspect $CNAME &> /dev/null ; then
     docker rm $CNAME
 fi
 
-NETWORKARGS="--network=wikispeech"
-docker run --name=$CNAME $NETWORKARGS -p 10000:10000 -v /home/hanna/git_repos/wikispeech_mockup/wikispeech_server/:/config/ -it wikispeech $*
+docker run --name=$CNAME -p 10000:10000 -v /home/hanna/git_repos/wikispeech_mockup/wikispeech_server/:/config/ -it wikispeech $*
 
 ## TODO: SNYGGARE MAPPNING AV CONFIGFILER
 
