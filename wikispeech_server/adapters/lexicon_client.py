193f0bad7acb2ca854ef44b252a9d7c902a2ccdc
HaraldBerthelsen <haraldberthelsen@gmail.com>
2021-05-25 18:46:54 +0200

    Changed test() warning message to include base_url

--- wikispeech_server/adapters/lexicon_client.py
+++ wikispeech_server/adapters/lexicon_client.py
@@ -196,7 +196,7 @@ class Lexicon(object):
             log.error(msg)
             raise LexiconException(msg)
         except Exception as e:
-            msg = "Unable to create lexicon client for %s at url %s. Reason: %s" % (self.lexicon_name, url, e)
+            msg = "Unable to create lexicon client for %s at url %s. Reason: %s" % (self.lexicon_name, self.base_url, e)
             log.warning(msg)
             raise LexiconException(msg)
 
