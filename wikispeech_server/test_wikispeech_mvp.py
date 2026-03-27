c782741ae2794dd381a115f32e83a12129fd4fba
nikolaj <nikolaj@stts.se>
2026-03-27 13:25:01 +0100

    test_wikispeech_mvp.py

--- wikispeech_server/test_wikispeech_mvp.py
+++ wikispeech_server/test_wikispeech_mvp.py
@@ -19,10 +19,23 @@ def test_aaa_ping_server(client, base_url):
 
 # Test that a few calls return 200
 
+    
 def test_root(client, base_url):
     response = client.get(f"{base_url}/")
     assert response.status_code == 200
-
+        
 def test_list_voices(client, base_url):
     response = client.get(f"{base_url}/synthesis/voices")
+    assert response.status_code == 200
+            
+def testNoSuchPath404(client, base_url):
+    response = client.get(f"{base_url}/gabbatgabbahey")
+    assert response.status_code == 404
+    
+def test_sv_se_nst_male1(client, base_url):
+    response = client.get(f"{base_url}/?lang=sv&input=Vi%20testar%20matcha%20talsyntes&voice=sv_se_nst_male1&input_type=text")
+    assert response.status_code == 200
+                
+def test_sv_vc_male_mart2nik_p(client, base_url):
+    response = client.get(f"{base_url}/?lang=sv&input=Ett+test+med+en+manlig+r%C3%B6st&voice=sv_vc_male_mart2nik_p")
     assert response.status_code == 200
