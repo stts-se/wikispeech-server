9b2a3f09ca0f0ffdef05e61b978894af7ba40628
Hanna Lindgren <han.lind@gmail.com>
2026-04-10 12:29:40 +0200

    pytest+CI tests calling all expected voices for mvp2

--- wikispeech_server/test_wikispeech_mvp2.py
+++ wikispeech_server/test_wikispeech_mvp2.py
@@ -1,11 +1,11 @@
-## This tests a running server, using the config_mvp.env et al. In other words, the Wikispeech server must be started before running these tests.
+## This tests a running server, using the config_mvp2.env et al. In other words, the Wikispeech server must be started before running these tests.
 
-# pytest test_wikispeech_mvp.py
+# pytest test_wikispeech_mvp2.py
 
 import pytest
 import requests
 
-class TestMVP:
+class TestMVP2:
 
     # First of all, ping the server.
     # ("_aaa_" so that this test appears
@@ -510,3 +510,53 @@ class TestMVP:
         }
         ]
         assert tokens_got == tokens_exp
+
+    def test_call_all_mvp2_voices(self,client, base_url):
+        test_data = {
+            "sv": {
+                "text": "Hej, jag talar god svenska naturligtvis.",
+                "voices": [
+                    # piper
+                    "sv_vc_m2m_p","sv_vc_m2f_p",
+                    # matcha
+                    "sv_vc_m2m","sv_vc_m2f"
+                ]
+            },
+            "en": {
+                "text": "Hello, I do speak some English as well.",
+                "voices": [
+                    # flite
+                    "cmu-slt-flite",
+                    # piper
+                    "en_US-bryce-medium", "en_US-ljspeech-high",
+                    # matcha
+                    "en_us_ljspeech", "en_us_vctk"
+                ]
+            }
+        }
+        for lang in test_data:
+            text = test_data[lang]["text"]
+            for voice in test_data[lang]["voices"]:
+                url = f"{base_url}/?lang={lang}&input={text}&voice={voice}"
+                response = client.get(url)
+                assert response.status_code == 200
+                assert not response.text.lower().startswith("error"), f"Server returned error: {response.text}"
+                data = response.json()
+                assert "audio" in data
+                assert "audio_data" in data
+                assert "tokens" in data
+                assert len(data["tokens"]) > 5
+                assert len(data["tokens"]) < 10
+                # Timestamps change for each run, but they should increase
+                t1 = data["tokens"][0]["endtime"]
+                t2 = data["tokens"][1]["endtime"]
+                assert t1 < t2
+                assert "adapter" in data["voice"]
+                assert "config_file" in data["voice"]
+                assert "engine" in data["voice"]    
+                assert "lang" in data["voice"]
+                assert "longname" in data["voice"]
+                if data["voice"]["engine"] in ["piper","matcha"] and lang == "sv":
+                    assert "mapper" in data["voice"]
+                assert "name" in data["voice"]
+
